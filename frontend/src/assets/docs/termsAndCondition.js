e3144ce2cf04490601b66bee1d4f13d618e0dc16
tarun Shori <tarunshori801@gmail.com>
2023-03-27 00:24:31 +0530

    initial commiit

---
+++ frontend/src/assets/docs/termsAndCondition.js
@@ -0,0 +1,24 @@
+const data = `
+
+
+
+// terms and condition here
+
+
+
+
+
+1. CONTACT US
+
+In order to resolve a complaint regarding the Site or to receive further information regarding use of the Site, please contact us at:
+
+Video Streaming
+asasddasd
+
+India
+Phone: __________
+official.Tarunshori801@gmail.com
+
+`;
+
+export default data;
